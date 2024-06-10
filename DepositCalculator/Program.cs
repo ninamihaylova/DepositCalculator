3581f45346bd39165961f348a5b5263fe843748e
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-06-10 23:20:11 +0300

    Initial commit

---
+++ DepositCalculator/Program.cs
@@ -0,0 +1,18 @@
+﻿class DepositCalculator
+{
+    static void Main(string[] args)
+
+    {
+        decimal depositAmount = decimal.Parse(Console.ReadLine());
+      int depositMonths = int.Parse(Console.ReadLine());
+
+        decimal annualInterestRate = decimal.Parse(Console.ReadLine());
+
+        decimal annualInterest = depositAmount * annualInterestRate/100;
+
+        decimal monthInterest = annualInterest/12;
+        decimal totalAmount = depositAmount + monthInterest*depositMonths;
+        
+        Console.WriteLine(totalAmount);
+    }
+    }
